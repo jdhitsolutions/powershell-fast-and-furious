0dfc70279ee1441d6850d2148b38fa80b2109e7b
Jeff Hicks <jhicks@jdhitsolutions.com>
2019-10-12 08:54:20 -0400

    initial

---
+++ demos.ps1
@@ -0,0 +1,26 @@
+return "This is a guided demo."
+
+#region Push/Pop paths
+
+#endregion
+
+#region Console aliases
+
+#endregion
+
+#region parenthetical expressions
+
+#endregion
+
+
+#region Custom type extensions
+
+#endregion
+
+#region Autocompleters
+
+#endregion
+
+#region Using PSReadline
+
+#endregion
